5e7cd6b5fe5a09cd4ead63c7faa8181cd36b268f
sparkle1511 <aakudale76@gmail.com>
2025-06-28 18:08:23 +0530

    Add files via upload

---
+++ attendTrackingSystem/src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/AttendTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package AttendanceTrackingSystem.com.attendTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ attendTrackingSystem/src/test/java/AttendanceTrackingSystem/com/attendTrackingSystem/AttendTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package AttendanceTrackingSystem.com.attendTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
